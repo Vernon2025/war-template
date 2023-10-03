f722d2ea966d23e047674e9c8510dc4b7901920b
Vernon2025 <143050322+Vernon2025@users.noreply.github.com>
2023-10-02 18:00:47 -0700

    Deck in progress

--- Deck.java
+++ Deck.java
@@ -1,5 +1,6 @@
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Collections;
 
 /**
  * Emulate a deck of cards
@@ -7,24 +8,22 @@ import java.util.ArrayList;
  * @author Mr. Jaffe
  * @version 2022-10-19
  */
-public class Deck
-{
+public class Deck {
     private List<Card> cards;
 
     /**
      * Deck constructor: Create an empty deck of cards
      */
-    public Deck()
-    {
+    public Deck() {
         cards = new ArrayList<Card>();
     }
-    
+
     public void initializeNewDeck() {
-        String[] suits = {"Hearts","Clubs","Spades","Diamonds"};
-        int[] ranks = {2,3,4,5,6,7,8,9,10,11,12,13,14};
-        String[] faces = {"2","3","4","5","6","7","8","9","10","Jack","Queen","King","Ace"};
+        String[] suits = { "Hearts", "Clubs", "Spades", "Diamonds" };
+        int[] ranks = { 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 };
+        String[] faces = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace" };
         for (String suit : suits) {
-            for (int idx=0; idx<ranks.length; idx++) {
+            for (int idx = 0; idx < ranks.length; idx++) {
                 Card c = new Card(ranks[idx], faces[idx], suit);
                 this.cards.add(c);
             }
@@ -39,18 +38,20 @@ public class Deck
     public int getDeckSize() {
         return cards.size();
     }
-    
+
     /**
      * Shuffles the cards in the deck
      */
     public void shuffle() {
         // To be written
+        Collections.shuffle(cards);
     }
-    
+
     /**
      * Deal all the cards in the deck to make two new decks of cards
      * 
-     * @returns Deck array where index 0 is the first deck and index 1 is the second deck
+     * @returns Deck array where index 0 is the first deck and index 1 is the second
+     *          deck
      */
     public Deck[] dealDeck() {
         Deck[] halves = new Deck[2];
@@ -63,22 +64,29 @@ public class Deck
         }
         return halves;
     }
-    
+
     /**
      * Deal the top card of the deck and remove it from the deck
+     * 
      * @returns The top card of the deck (at cards index 0)
      */
     public Card dealCardFromDeck() {
-        // To be written 
-        return null;
+        // To be written
+        try {
+            return cards.remove(0);
+        } catch (IndexOutOfBoundsException e) {
+            return null;
+        }
     }
-    
+
     /**
      * Adds the provided card to the deck
+     * 
      * @param cardToAdd: Card to add to this deck
      */
     public void addCardToDeck(Card cardToAdd) {
         // To be written
+        cards.add(cardToAdd);
     }
-    
+
 }
